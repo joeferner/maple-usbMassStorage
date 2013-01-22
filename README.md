dd744527872a3c019c2bcfd2d89d44ee35532edb
Joe Ferner <joe@fernsroth.com>
2013-01-22 11:18:42 -0500

    readme and example

---
+++ README.md
@@ -0,0 +1,161 @@
+
+# maple-usbMassStorage
+
+Driver to allow [LeafLabs maple](http://leaflabs.com/devices/maple/) act as a USB mass storage device.
+
+# API
+
+* [USBMassStorage.begin()](#USBMassStorage_begin)
+* [USBMassStorage.loop()](#USBMassStorage_loop)
+* [MAL_massBlockCount[2] : uint32_t](#massBlockCount)
+* [MAL_massBlockSize[2] : uint32_t](#massBlockSize)
+* [usb_mass_mal_init(uint8_t lun) : uint16_t](#usb_mass_mal_init)
+* [usb_mass_mal_get_status(uint8_t lun) : uint16_t](#usb_mass_mal_get_status)
+* [usb_mass_mal_write_memory(uint8_t lun, uint32_t memoryOffset, uint8_t *writebuff, uint16_t transferLength) : uint16_t](#usb_mass_mal_write_memory)
+* [usb_mass_mal_read_memory(uint8_t lun, uint32_t memoryOffset, uint8_t *readbuff, uint16_t transferLength) : uint16_t](#usb_mass_mal_read_memory)
+* [usb_mass_mal_format() : void](#usb_mass_mal_format)
+
+# API Details
+
+<a name="USBMassStorage_begin" />
+**USBMassStorage.begin()**
+
+Starts the USB mass storage driver. After calling this method the computer should now detect the Maple as a USB mass
+storage device.
+
+<a name="USBMassStorage_loop" />
+**USBMassStorage.loop()**
+
+Allows the USB mass storage driver to process incoming requests. Call this method in your main loop.
+
+Even though the STM32 USB causes interrupts which could be used to process this data. Typically interrupts should be
+kept short. The other side effect is that certain libraries do not work inside the interrupt handlers (such as sdfat).
+Using the loop function solves these problems by moving most of the logic into non-interrupt code.
+
+__Example__
+
+```
+void loop() {
+  USBMassStorage.loop();
+}
+```
+
+<a name="massBlockCount" />
+**MAL_massBlockCount[2] : uint32_t**
+
+The block count of the two SCSI LUNs. This should be initialized before [USBMassStorage.begin()](#USBMassStorage_begin).
+
+__Example__
+
+```
+uint32_t MAL_massBlockCount[2];
+uint32_t MAL_massBlockSize[2];
+
+void setup() {
+  MAL_massBlockCount[0] = 100;
+  MAL_massBlockCount[1] = 0;
+  MAL_massBlockSize[0] = 512;
+  MAL_massBlockSize[1] = 0;
+
+  USBMassStorage.begin();
+}
+```
+
+<a name="massBlockSize" />
+**MAL_massBlockSize[2] : uint32_t**
+
+The block size of the two SCSI LUNs. This should be initialized before [USBMassStorage.begin()](#USBMassStorage_begin).
+For SDCards you should initialize it to 512.
+
+<a name="usb_mass_mal_init" />
+**usb_mass_mal_init(uint8_t lun) : uint16_t**
+
+Called by the USB mass storage driver after the host has requested the device.
+
+__Arguments__
+
+ * lun - the LUN number being initialized. This can be 0 or 1. Typically just 0.
+
+__Return__
+
+0 for success. 1 for failure.
+
+<a name="usb_mass_mal_get_status" />
+**usb_mass_mal_get_status(uint8_t lun) : uint16_t**
+
+Gets the status of the given LUN.
+
+__Arguments__
+
+ * lun - the lun number to get status for. This can be 0 or 1. Typically just 0.
+
+__Return__
+
+0 for success. 1 for failure.
+
+<a name="usb_mass_mal_write_memory" />
+**usb_mass_mal_write_memory(uint8_t lun, uint32_t memoryOffset, uint8_t *writebuff, uint16_t transferLength) : uint16_t**
+
+Called when data should be written to storage.
+
+__Arguments__
+
+ * lun - the lun number to write to. This can be 0 or 1. Typically just 0.
+ * memoryOffset - Address in bytes to write to.
+ * writebuff - A buffer containing the data to write. This buffer should not be relied on after this function returns as
+               it may be reused.
+ * transferLength - The number of bytes to write.
+
+__Return__
+
+USB_MASS_MAL_SUCCESS for success. USB_MASS_MAL_FAIL for failure.
+
+__Example__
+
+```
+extern "C" uint16_t usb_mass_mal_write_memory(uint8_t lun, uint32_t memoryOffset, uint8_t *writebuff, uint16_t transferLength) {
+  uint32_t block = memoryOffset / 512;
+  if (lun != 0) {
+    return USB_MASS_MAL_FAIL;
+  }
+  if (sdcard.writeBlock(block, writebuff)) {
+    return USB_MASS_MAL_SUCCESS;
+  }
+  return USB_MASS_MAL_FAIL;
+}
+```
+
+<a name="usb_mass_mal_read_memory" />
+**usb_mass_mal_read_memory(uint8_t lun, uint32_t memoryOffset, uint8_t *readbuff, uint16_t transferLength) : uint16_t**
+
+Called when data should be read from storage.
+
+__Arguments__
+
+ * lun - the lun number to write to. This can be 0 or 1. Typically just 0.
+ * memoryOffset - Address in bytes to read from.
+ * readbuff - A buffer in which the data should be read into.
+ * transferLength - The number of bytes to read.
+
+__Return__
+
+USB_MASS_MAL_SUCCESS for success. USB_MASS_MAL_FAIL for failure.
+
+__Example__
+
+```
+extern "C" uint16_t usb_mass_mal_read_memory(uint8_t lun, uint32_t memoryOffset, uint8_t *readbuff, uint16_t transferLength) {
+  if (lun != 0) {
+    return USB_MASS_MAL_FAIL;
+  }
+  if (sdcard.readBlock(memoryOffset / 512, readbuff)) {
+    return USB_MASS_MAL_SUCCESS;
+  }
+  return USB_MASS_MAL_FAIL;
+}
+```
+
+<a name="usb_mass_mal_format" />
+**usb_mass_mal_format() : void**
+
+Called to format the device.
--- examples/sdcard.cpp
+++ examples/sdcard.cpp
@@ -1,46 +1,58 @@
 
-/*
- * export GLOBAL_CFLAGS=-DDISABLE_SERIALUSB
- * export USER_MODULES="./libraries/sdcard ./libraries/usbMassStorage"
- * make
- */
-
 #include <wirish/wirish.h>
 #include <usbMassStorage/usbMassStorage.h>
-#include <sdcard/Sd2Card.h>
+#include <sdfat/Sd2Card.h>
+#include <sdfat/SdFat.h>
 
+/**
+ * This example requires the maple-sdfat library (http://code.google.com/p/maple-sdfat/).
+ * 
+ * When the example starts it creates two files /PROC/REBOOT and /PROC/BOOTLOAD
+ * If you write anything to these files from the host computer it will reboot or
+ * enter the bootloader respectively.
+ * 
+ *   Connections:
+ *      D10             -> SDCard.CS
+ *      D11 (SPI1 MOSI) -> SDCard.DI
+ *      D12 (SPI1 MISO) -> SDCard.DO
+ *      D13 (SPI1 SCK)  -> SDCard.CLK
+ */
 #define SDCARD_CS  D10
 #define SDCARD_SPI 1
 
 uint32_t MAL_massBlockCount[2];
 uint32_t MAL_massBlockSize[2];
+uint32_t rebootFileBlock;
+uint32_t bootLoadFileBlock;
 
 HardwareSPI spi1(SDCARD_SPI);
-Sd2Card sdcard(spi1, SDCARD_CS);
+Sd2Card sdcard(spi1);
+SdVolume sdVolume;
+
+uint8_t initSdCardSize();
+uint8_t initCard();
+void reboot();
+void enterBootloader();
 
 void setup() {
-  delay(1000);
+  delay(1000); // not needed just easier to debug
   Serial1.begin(9600);
   Serial1.println("begin");
-  pinMode(BOARD_LED_PIN, OUTPUT);
   spi1.begin(SPI_QUARTER_SPEED, MSBFIRST, 0);
-  sdcard.begin();
+  sdcard.init(SPI_QUARTER_SPEED, SDCARD_CS);
+  sdVolume.init(&sdcard, 0);
 
-  Serial1.println("getCardSize");
-  MAL_massBlockCount[0] = sdcard.getCardSize();
-  MAL_massBlockCount[1] = 0;
-  MAL_massBlockSize[0] = 512;
-  MAL_massBlockSize[1] = 0;
-  Serial1.print("cardSize = ");
-  Serial1.println((uint32_t) MAL_massBlockCount[0] * 512);
+  initSdCardSize();
+  initCard();
 
+  Serial1.println("USBMassStorage.begin");
   USBMassStorage.begin();
+
+  Serial1.println("setup complete");
 }
 
 void loop() {
   USBMassStorage.loop();
-  //togglePin(BOARD_LED_PIN);
-  //delay(1000);
 }
 
 // Force init to be called *first*, i.e. before static object allocation.
@@ -60,37 +72,107 @@ int main(void) {
   return 0;
 }
 
+/**
+ * Initializes the USBMassStorage driver with the size of the SDCard. 
+ */
+uint8_t initSdCardSize() {
+  Serial1.println("initSdCardSize");
+  uint32_t numberOfBlocks = sdcard.cardSize();
+  if (numberOfBlocks == 0) {
+    Serial1.println("could not get card size");
+    return false;
+  }
+  MAL_massBlockCount[0] = numberOfBlocks;
+  MAL_massBlockCount[1] = 0;
+  MAL_massBlockSize[0] = 512;
+  MAL_massBlockSize[1] = 0;
+  Serial1.print("cardSize = ");
+  Serial1.println(numberOfBlocks * 512);
+  return true;
+}
+
+/**
+ * Creates the /PROC/REBOOT and /PROC/BOOTLOAD files to allow
+ * remote reboot and bootloader respectively.
+ */
+uint8_t initCard() {
+  Serial1.println("initCard");
+  SdFile root, procDir, rebootFile, bootLoadFile;
+
+  // open the root directory
+  if (!root.openRoot(&sdVolume)) {
+    Serial1.println("failed to openRoot");
+    return false;
+  }
+
+  // open the /PROC directory
+  if (!procDir.open(root, "PROC", O_READ)) {
+    if (!procDir.makeDir(root, "PROC")) {
+      Serial1.println("failed to create proc");
+      return false;
+    }
+  }
+
+  // create /PROC/REBOOT file and write a '0' to it.
+  if (!rebootFile.open(procDir, "REBOOT", O_WRITE | O_CREAT | O_TRUNC)) {
+    Serial1.println("Could not create /PROC/REBOOT");
+    return false;
+  }
+  rebootFile.write('0');
+  rebootFile.close();
+  rebootFileBlock = rebootFile.firstCluster() * sdVolume.blocksPerCluster();
+  Serial1.print("/PROC/REBOOT: ");
+  Serial1.println(rebootFileBlock);
+
+  // create /PROC/BOOTLOAD file and write a '0' to it.
+  if (!bootLoadFile.open(procDir, "BOOTLOAD", O_WRITE | O_CREAT | O_TRUNC)) {
+    Serial1.println("Could not create /PROC/BOOTLOAD");
+    return false;
+  }
+  bootLoadFile.write('0');
+  bootLoadFile.close();
+  bootLoadFileBlock = bootLoadFile.firstCluster() * sdVolume.blocksPerCluster();
+  Serial1.print("/PROC/BOOTLOAD: ");
+  Serial1.println(bootLoadFileBlock);
+
+  procDir.close();
+  root.close();
+  return true;
+}
+
+/*
+ * The following methods are used by the USBMassStorage driver to read and write to the SDCard.  
+ */
+
 extern "C" uint16_t usb_mass_mal_init(uint8_t lun) {
   return 0;
 }
 
 extern "C" uint16_t usb_mass_mal_get_status(uint8_t lun) {
-  return sdcard.getErrorCode();
+  return sdcard.errorCode();
 }
 
 extern "C" uint16_t usb_mass_mal_write_memory(uint8_t lun, uint32_t memoryOffset, uint8_t *writebuff, uint16_t transferLength) {
-  //  Serial1.print("usb_mass_mal_write_memory(");
-  //  Serial1.print(memoryOffset);
-  //  Serial1.print(",");
-  //  Serial1.print(transferLength);
-  //  Serial1.println(")");
+  uint32_t block = memoryOffset / 512;
+  if (block == rebootFileBlock) {
+    reboot();
+    return USB_MASS_MAL_SUCCESS;
+  }
+  if (block == bootLoadFileBlock) {
+    enterBootloader();
+    return USB_MASS_MAL_SUCCESS;
+  }
 
   if (lun != 0) {
     return USB_MASS_MAL_FAIL;
   }
-  if (sdcard.writeBlock(memoryOffset / 512, writebuff)) {
+  if (sdcard.writeBlock(block, writebuff)) {
     return USB_MASS_MAL_SUCCESS;
   }
   return USB_MASS_MAL_FAIL;
 }
 
 extern "C" uint16_t usb_mass_mal_read_memory(uint8_t lun, uint32_t memoryOffset, uint8_t *readbuff, uint16_t transferLength) {
-  //  Serial1.print("usb_mass_mal_read_memory(");
-  //  Serial1.print(memoryOffset);
-  //  Serial1.print(",");
-  //  Serial1.print(transferLength);
-  //  Serial1.println(")");
-
   if (lun != 0) {
     return USB_MASS_MAL_FAIL;
   }
@@ -101,4 +183,54 @@ extern "C" uint16_t usb_mass_mal_read_memory(uint8_t lun, uint32_t memoryOffset,
 }
 
 extern "C" void usb_mass_mal_format() {
+}
+
+/*
+ * Helper methods to reboot and enter the bootloader 
+ */
+
+void reboot() {
+  Serial1.println("reset");
+  nvic_sys_reset();
+  while (1);
+}
+
+#define RESET_DELAY 100000
+
+static void wait_reset(void) {
+  delay_us(RESET_DELAY);
+  nvic_sys_reset();
+}
+
+#define STACK_TOP 0x20000800
+#define EXC_RETURN 0xFFFFFFF9
+#define DEFAULT_CPSR 0x61000000
+
+void enterBootloader() {
+  Serial1.println("bootloader");
+
+  // Got the magic sequence -> reset, presumably into the bootloader.
+  // Return address is wait_reset, but we must set the thumb bit.
+  uintptr_t target = (uintptr_t) wait_reset | 0x1;
+  asm volatile("mov r0, %[stack_top]      \n\t" // Reset stack
+            "mov sp, r0                \n\t"
+            "mov r0, #1                \n\t"
+            "mov r1, %[target_addr]    \n\t"
+            "mov r2, %[cpsr]           \n\t"
+            "push {r2}                 \n\t" // Fake xPSR
+            "push {r1}                 \n\t" // PC target addr
+            "push {r0}                 \n\t" // Fake LR
+            "push {r0}                 \n\t" // Fake R12
+            "push {r0}                 \n\t" // Fake R3
+            "push {r0}                 \n\t" // Fake R2
+            "push {r0}                 \n\t" // Fake R1
+            "push {r0}                 \n\t" // Fake R0
+            "mov lr, %[exc_return]     \n\t"
+            "bx lr"
+            :
+            : [stack_top] "r" (STACK_TOP),
+            [target_addr] "r" (target),
+            [exc_return] "r" (EXC_RETURN),
+            [cpsr] "r" (DEFAULT_CPSR)
+            : "r0", "r1", "r2");
 }
